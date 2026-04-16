bef02ae09921fab0ae387d8a562ef6804f31a415
Jairo Ambres Rodriguez <ambresrodriguez@gmail.com>
2026-04-16 14:06:08 +0200

    Arreglo de un error en base de datos y de error en programación+ elaboracion de informe

--- BaseDeDatos/proyecto.sql
+++ BaseDeDatos/proyecto.sql
@@ -42,12 +42,18 @@ insert into usuarios values(2,'usuario2','usuario2@gmail.com','usuario2');
 insert into usuarios values(3,'usuario3','usuario3@gmail.com','usuario3');
 insert into usuarios values(4,'usuario4','usuario4@gmail.com','usuario4');
 
-insert into eventos values(1,'15-06-2026','constelaciones','gijón','Una obra de teatro contemporánea que explora la relacion entre una fìsica cuántica y un apicultor');
-insert into eventos values(2,'05-06-2026','mujeres en la musica','gijón','Exposicion que reúne imágenes y materiales de artistas como Rocio Jurado o Aitana');
-insert into eventos values(3,'02-10-2026','the silence','gijón','Para su obra autoficticoa el dramaturgo y director Flak Richter se centra en su propia historia familiar');
-insert into eventos values(4,'06-04-2026','fiesta del bollo','avilés','celebración primaveral con desfiles, comida en la calle y ambiente festivo que cierra la Semana Santa.');
-insert into eventos values(5,'24-03-2026','mamma mia','teatro jovellanos, gijón','comedia romántica con canciones de ABBA sobre una boda y un misterio familiar.');
-insert into eventos values(6,'01-01-2026','oviedo-español','estadio municipal carlos tartiere, oviedo','un partido de liga entre el oviedo y el español');
+insert into eventos values(1,'15-06-2026','constelaciones','gijón','Una obra de teatro contemporánea que 
+                                    explora la relacion entre una fìsica cuántica y un apicultor');
+insert into eventos values(2,'05-06-2026','mujeres en la musica','gijón','Exposicion que reúne imágenes 
+                                    y materiales de artistas como Rocio Jurado o Aitana');
+insert into eventos values(3,'02-10-2026','the silence','gijón','Para su obra autoficticoa
+                            el dramaturgo y director Flak Richter se centra en su propia historia familiar');
+insert into eventos values(4,'06-04-2026','fiesta del bollo','avilés','celebración primaveral
+                        con desfiles, comida en la calle y ambiente festivo que cierra la Semana Santa.');
+insert into eventos values(5,'24-03-2026','mamma mia','teatro jovellanos, gijón','comedia 
+                                romántica con canciones de ABBA sobre una boda y un misterio familiar.');
+insert into eventos values(6,'01-01-2026','oviedo-español','estadio municipal carlos tartiere, oviedo','un 
+                                                        partido de liga entre el oviedo y el español');
 insert into eventos values(7,'12-01-2026','espectaculo drones','playa de San Lorenzo, gijon','Un evento que llenará nuestros días de asombro y emoción. Durante estos días inolvidables, nuestro cielo se convertirá en el escenario de un espectáculo impresionante, con actividades para toda la familia.');
 insert into eventos values(8,'24-01-2026','monoloco fest','pabellón de la Magdalena, aviles','Un festival de musica urbana que se celebra en diferentes ciudades de españa');
 insert into eventos values(9,'25-06-2026','exposicion silvia cruz','Aviles','Nueva y originales canciones para compartir, con sonidos familiares y nuevos horizontes');
@@ -89,13 +95,14 @@ from galerias
 join eventos on id_even = id_gal
 where fecha < ('28-02-2026');
 
-select * from galerias_antiguas;
+
 
 create view evento_favorito as
 select eventos.titulo
 from eventos
 join favoritos on id_even = id_evento
 where id_usuario = 1;
+
 
 create view devolver_imagen as
 select imagenes.titulo
@@ -104,6 +111,7 @@ join galerias on id_galeria = id_gal
 join eventos on id_gal= id_even
 where fecha = ('12-01-2026');
 
+
 create view eventos_favoritos as
 select *
 from eventos
@@ -111,6 +119,7 @@ join favoritos on id_even = id_evento
 where id_usuario = 2
 and fecha > ('28-02-2026');
 
+select * from eventos_favoritos;
 
 
 
